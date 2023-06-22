50b60ea9e4d0139dbb080c56a7ca642f0632426d
alantothe <rubenamalpartida@gmail.com>
2023-06-22 14:54:22 -0400

    installed packages, set up tailwind, set up react-router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+
+import Dashboard from './components/Dashboard/Dashboard';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegistrationPage from './components/RegistrationPage/RegistrationPage';
+import UpdateProfile from './components/UpdateProfile/UpdateProfile';
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<>
+    <Router>
+      <Routes>
+        <Route  path="/login" element={<LoginPage />} />
+        <Route  path="/register" element={<RegistrationPage />} />
+        <Route  path="/dashboard" element={<Dashboard />} />
+        <Route  path="/user" element={<UpdateProfile />} />
+        <Route path="/" element={<Navigate to="/dashboard" />} />
+      </Routes>
+    </Router>
+    </>
     </div>
   );
 }
---
+++ src/components/UpdateProfile/UpdateProfile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function UpdateProfile() {
+  return (
+    <div>
+    <h1>Update Profile Page</h1>
+
+    </div>
+  )
+}
+
+export default UpdateProfile
