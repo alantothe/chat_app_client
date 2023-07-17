2bec8cd19a9dd59660372f8e6bd53203ed5d0482
alantothe <rubenamalpartida@gmail.com>
2023-07-17 14:55:55 -0400

    css

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import SideUserBar from './components/SideUserBar'
+import MainSideBar from './components/MainSideBar'
+import ChatBox from './components/ChatBox'
+import Friends from './components/Friends'
 
 function Dashboard() {
   return (
-    <div>
-    <h1> Dashboard Page</h1>
+    <div className='flex flex-row w-screen h-screen m-0'>
+
+      <div className='w-1/12'>
+        <SideUserBar/>
+      </div>
+
+      <div className='w-1/5'>
+        <MainSideBar/>
+      </div>
+
+      <div className='w-8/12'>
+        <ChatBox />
+      </div>
+
+      <div className='w-1/5'>
+        <Friends/>
+      </div>
 
     </div>
   )
---
+++ src/components/Dashboard/components/ChatBox.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ChatBox() {
+  return (
+    <div className='h-full bg-zinc-800 text-white flex items-center justify-center'>
+      <h1>Chat Box</h1>
+    </div>
+  )
+}
+
+export default ChatBox
